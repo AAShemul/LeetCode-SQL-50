31a140f0a2a0cc0d44215134e72ba6cfcfdad069
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2024-04-30 11:25:11 +0600

    Update

---
+++ src/02. Basic Joins/11. 577 - Employee Bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT E.name,
+       B.bonus
+FROM Employee E
+         LEFT JOIN Bonus B
+                   ON E.empId = B.empId
+WHERE B.bonus < 1000
+   OR B.bonus IS NULL
